6a7b5d36a5289c4a944333d394ae45eb7b1ae7fd
gcastro95 <gabriel.alfonso.castro@gmail.com>
2021-07-26 10:05:14 +0100

    header checks

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, abort, make_response, jsonify, Response
+import hmac
+import hashlib
 
 app = Flask(__name__)
 
@@ -6,10 +8,20 @@ app = Flask(__name__)
 def webhook(type):
     if type == "twitch":
         print(request.json)
-        print(request.headers)
-        data = request.json['challenge']
-        print(data)
-        return make_response(data, 201)
+        headers = request.headers
+        if headers["Twitch-Eventsub-Message-Type"] == "webhook_callback_verification":
+            challenge = request.json['challenge']
+            print(challenge)
+            return make_response(challenge, 201)
+        elif headers["Twitch-Eventsub-Message-Type"] == "notification":
+            message = headers["Twitch-Eventsub-Message-Id"] + headers["Twitch-Eventsub-Message-Timestamp"] + request.json
+            signature = hmac.new("wh474r3y0ubl1nd", message, digestmod=hashlib.sha256).hexdigest()
+            expected_signature = "sha256=" + signature
+            print(expected_signature)
+            if headers["Twitch-Eventsub-Message-Signature"] != expected_signature:
+                return 403
+            else:
+                return 201
 
 if __name__ == '__main__':
     app.run(ssl_context='adhoc', debug=True, port=443)
