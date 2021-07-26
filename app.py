552924f173d3cd7c286ed1751dd9bc60ba5f6474
gcastro95 <gabriel.alfonso.castro@gmail.com>
2021-07-26 14:47:12 +0100

    requirements

--- app.py
+++ app.py
@@ -8,21 +8,23 @@ app = Flask(__name__)
 @app.route('/webhook/<type>', methods=['GET', 'POST'])
 def webhook(type):
     if type == "twitch":
-        challenge = request.json['challenge']
-        if challenge:
-            return make_response(challenge, 201)
         headers = request.headers
-        message = headers["Twitch-Eventsub-Message-Id"] + headers["Twitch-Eventsub-Message-Timestamp"] + str(request.get_data(True, True, False))
-        key = bytes("wh474r3y0ubl1nd", "utf-8")
-        data = bytes(message, "utf-8")
-        signature = hmac.new(key, data, digestmod=hashlib.sha256)
-        expected_signature = "sha256=" + signature.hexdigest()
-        if headers["Twitch-Eventsub-Message-Signature"] != expected_signature:
-            print("it worked but it didn't get accepted")
-            return make_response("failed", 403)
-        else:
-            print("it worked bitch!")
-            return make_response("success", 201)
+        if headers["Twitch-Eventsub-Message-Type"] == "webhook_callback_verification":
+            challenge = request.json['challenge']
+            if challenge:
+                return make_response(challenge, 201)
+        elif headers["Twitch-Eventsub-Message-Type"] == "notification":
+            message = headers["Twitch-Eventsub-Message-Id"] + headers["Twitch-Eventsub-Message-Timestamp"] + str(request.get_data(True, True, False))
+            key = bytes("wh474r3y0ubl1nd", "utf-8")
+            data = bytes(message, "utf-8")
+            signature = hmac.new(key, data, digestmod=hashlib.sha256)
+            expected_signature = "sha256=" + signature.hexdigest()
+            if headers["Twitch-Eventsub-Message-Signature"] != expected_signature:
+                print("it worked but it didn't get accepted")
+                return make_response("failed", 403)
+            else:
+                print("it worked bitch!")
+                return make_response("success", 201)
 
     elif type == "youtube":
         challenge = request.args.get("hub.challenge")
