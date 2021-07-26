15f388699610e32c8f1beae66d7aa5c6d28e4850
gcastro95 <gabriel.alfonso.castro@gmail.com>
2021-07-26 11:35:52 +0100

    bytes instead of json

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def webhook(type):
             print(challenge)
             return make_response(challenge, 201)
         elif headers["Twitch-Eventsub-Message-Type"] == "notification":
-            message = headers["Twitch-Eventsub-Message-Id"] + headers["Twitch-Eventsub-Message-Timestamp"] + str(request.get_json())
+            message = headers["Twitch-Eventsub-Message-Id"] + headers["Twitch-Eventsub-Message-Timestamp"] + str(request.get_data(True, True, False))
             key = bytes("wh474r3y0ubl1nd", "utf-8")
             data = bytes(message, "utf-8")
             signature = hmac.new(key, data, digestmod=hashlib.sha256)
