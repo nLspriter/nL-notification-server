815ee3573384bc45b941a0fe8d299efae028c2a1
gcastro95 <gabriel.alfonso.castro@gmail.com>
2021-07-26 09:37:20 +0100

    variable subroute app.py commit

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, request, abort, make_response, jsonify, Response
 
 app = Flask(__name__)
 
-@app.route('/webhook/twitch', methods=['POST'])
-def webhook():
-    print(request.json)
-    print(request.headers)
-    data = request.json['challenge']
-    print(data)
-    return make_response(data, 201)
+@app.route('/webhook/<type>', methods=['POST'])
+def webhook(type):
+    if type == "twitch":
+        print(request.json)
+        print(request.headers)
+        data = request.json['challenge']
+        print(data)
+        return make_response(data, 201)
 
 if __name__ == '__main__':
     app.run(ssl_context='adhoc', debug=True, port=443)
