af95b839136c91f1f8ea85659a5f879cfa3be649
nLspriter <nlspriter@gmail.com>
2021-08-19 13:27:05 +0100

    IG Challenge Handler

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ from random import choice
 from string import ascii_letters
 import cv2
 import instagrapi
+import email
+import imaplib
+import re
 
 app = Flask(__name__)
 
@@ -161,6 +164,55 @@ def thumbnail(url):
     else:
         print("Unable to download image")
 
+def get_code_from_email(username):
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(os.environ.get("INSTAGRAM_EMAIL_ACCOUNT"), os.environ.get("INSTAGRAM_EMAIL_PASSWORD"))
+    mail.select("inbox")
+    result, data = mail.search(None, "(UNSEEN)")
+    assert result == "OK", "Error1 during get_code_from_email: %s" % result
+    ids = data.pop().split()
+    for num in reversed(ids):
+        mail.store(num, "+FLAGS", "\\Seen")  # mark as read
+        result, data = mail.fetch(num, "(RFC822)")
+        assert result == "OK", "Error2 during get_code_from_email: %s" % result
+        msg = email.message_from_string(data[0][1].decode())
+        payloads = msg.get_payload()
+        if not isinstance(payloads, list):
+            payloads = [msg]
+        code = None
+        for payload in payloads:
+            body = payload.get_payload(decode=True).decode()
+            if "<div" not in body:
+                continue
+            match = re.search(">([^>]*?({u})[^<]*?)<".format(u=username), body)
+            if not match:
+                continue
+            print("Match from email:", match.group(1))
+            match = re.search(r">(\d{6})<", body)
+            if not match:
+                print('Skip this email, "code" not found')
+                continue
+            code = match.group(1)
+        return code
+    return False
+
+
+def get_code_from_sms(username):
+    while True:
+        code = input(f"Enter code (6 digits) for {username}: ").strip()
+        if code and code.isdigit():
+            return code
+    return None
+
+
+def challenge_code_handler(username, choice):
+    if choice == 0:
+        return get_code_from_sms(username)
+    elif choice == 1:
+        return get_code_from_email(username)
+    return False
+
+
 @app.route("/status", methods=["GET"])
 def status():
     data = {
@@ -270,4 +322,5 @@ def webhook(type):
     return make_response("success", 201)
 
 if __name__ == "__main__":
+    igclient.challenge_code_handler = challenge_code_handler
     app.run(ssl_context="adhoc", debug=True, port=443)
