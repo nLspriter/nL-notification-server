e222d17cb65a87d8e5b7b5d0d796b8415a3eb312
gacastro95 <52107943+GACastro95@users.noreply.github.com>
2022-03-23 12:43:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -116,12 +116,12 @@ def post_youtube():
 
 @app.route("/subscribe-twitch/<token>", methods=["POST"])
 def subscribe_twitch(token):
-    subscribe_topic("twitch-browser", token)
+    subscribe_topic("twitch", token)
     return make_response("success", 201)
 
 @app.route("/unsubscribe-twitch/<token>", methods=["POST"])
 def unsubscribe_twitch(token):
-    unsubscribe_topic("twitch-browser", token)
+    unsubscribe_topic("twitch", token)
     return make_response("success", 201)
 
 @app.route("/subscribe-youtube/<token>", methods=["POST"])
